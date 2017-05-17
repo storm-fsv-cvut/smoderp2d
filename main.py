fb710fac0ed2e98c2cdd39b29ff588068316eafe
kavkapet <petr.kavka@fsv.cvut.cz>
2017-05-17 15:01:01 +0200

    mujmain - nechápu, neměl by se "porovnávat"

--- main.py
+++ main.py
@@ -25,8 +25,8 @@
 
 
 
-## @package main resolves some input variables and start the computing 
-#  
+## @package main resolves some input variables and start the computing
+#
 #  The computing itself is performed in main_src.runoff
 
 
@@ -34,7 +34,6 @@
 
 import sys
 import os
-import platform
 from    main_src.tools.tools import logical_argv
 from    main_src.tools.tools import get_argv
 import  main_src.constants   as constants
@@ -47,50 +46,43 @@ shallowrillstreamsurface
 surfaceandsubsurfacestreamflow
 '''
 
-  
+
 print "--------------------- INPUT PARAMETERS ---------------------"
 for item in sys.argv:
   print item
-print "--------------------- ---------------- ---------------------"; print 
-
-
-
-#if platform.system() == "Linux" :
-#  os.system('sledujproces.sh ' + str(os.getpid()) + ' smod > '+str(os.getpid())+'.txt  &' )
-  
+print "--------------------- ---------------- ---------------------"; print
 
 
 if __name__ == "__main__":
-  
+
+
   logical_argv(constants.PARAMETER_ARCGIS)
-  if sys.argv[constants.PARAMETER_EXTRA_OUTPUT+1] != '-' :
-    logical_argv(constants.PARAMETER_EXTRA_OUTPUT)
-  if sys.argv[constants.PARAMETER_MFDA+1] != '-' :
-    logical_argv(constants.PARAMETER_MFDA)
-  
-  
-  
+  logical_argv(constants.PARAMETER_EXTRA_OUTPUT)
+  logical_argv(constants.PARAMETER_MFDA)
+
+
+
   #sys.argv.append(sys.path[0]+os.sep+'indata/dp_hodnebodu_hodnemaletau.save')
   #sys.argv.append(sys.path[0]+os.sep+'indata/dp_hodne_bodu_tok.save')
-  #sys.argv.append(sys.path[0]+os.sep+'indata/parbunek_hodnebodu_maletau.save')
+
+  #sys.argv.append(sys.path[0]+os.sep+'DS_plochamalyNx.save')
+
+  sys.argv.append(sys.path[0]+os.sep+'byk.save')
   # full - full computation
   # dpre - data_preparation
   # roff - runoff
-  #sys.argv.append('roff')
+  sys.argv.append('roff')
   #sys.argv.append('dpre')
   #sys.argv.append('full')
-  
-  #sys.argv.append('false')
-  #logical_argv(constants.PARAMETER_DEBUG_PRT)
-  #print sys.argv[constants.PARAMETER_DEBUG_PRT+2]
-  
+
+  sys.argv.append('true'); logical_argv(constants.PARAMETER_DEBUG_PRT)
+
+
+
   #jj tady se nastavuje max_delta_t
-  #sys.argv.append(30.0)
-  
-  #print constants.PARAMETER_MAX_DELTA_T
-  #sys.exit()
-  
-  sys.argv.append('prt_times.in')
-  #sys.argv.append('')
+  #sys.argv.append(1.0)
+
+
+  sys.argv.append(sys.path[0]+os.sep+'prt_times.in')
   import main_src.runoff
 
