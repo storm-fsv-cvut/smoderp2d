52ca8906bb98d2dcefd5bb274f379493f63bc250
kavkapet <petr.kavka@fsv.cvut.cz>
2017-06-13 21:33:23 +0200

    uprava main.py

--- main.py
+++ main.py
@@ -67,23 +67,15 @@ if __name__ == "__main__":
 
   #sys.argv.append(sys.path[0]+os.sep+'DS_plochamalyNx.save')
 
-<<<<<<< HEAD
-  sys.argv.append(sys.path[0]+os.sep+'bykxx.save')
-=======
-  #sys.argv.append(sys.path[0]+os.sep+'byk.save')
->>>>>>> origin/pokusvyhoditratio
+  sys.argv.append(sys.path[0]+os.sep+'bykxxx.save')
   # full - full computation
   # dpre - data_preparation
   # roff - runoff
-  #sys.argv.append('roff')
-<<<<<<< HEAD
-  sys.argv.append('dpre')
-=======
+  sys.argv.append('roff')
   #sys.argv.append('dpre')
->>>>>>> origin/pokusvyhoditratio
   #sys.argv.append('full')
 
-  #sys.argv.append('true'); logical_argv(constants.PARAMETER_DEBUG_PRT)
+  sys.argv.append('true'); logical_argv(constants.PARAMETER_DEBUG_PRT)
 
 
 
@@ -91,6 +83,6 @@ if __name__ == "__main__":
   #sys.argv.append(1.0)
 
 
-  sys.argv.append('')
+  sys.argv.append(sys.path[0]+os.sep+'prt_times.in')
   import main_src.runoff
 
