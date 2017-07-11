be332721ba45210a57452c43eff1ed75f77db74f
jerabekjak@email.cz <jerabekjak>
2017-07-11 22:44:16 +0200

    mazu volani preparedata v datapreparation

--- main_src/data_preparation.py
+++ main_src/data_preparation.py
@@ -983,30 +983,3 @@ def prepare_data(args):
 
 
 
-
-
-boundaryRows, boundaryCols, mat_boundary, rrows, rcols, outletCells, x_coordinate, y_coordinate,\
-    NoDataValue, array_points, \
-    cols, rows, combinatIndex, delta_t,  \
-    mat_pi, mat_ppl, \
-    surface_retention, mat_inf_index, mat_hcrit, mat_aa, mat_b, mat_ret,\
-    mat_fd, mat_dmt, mat_efect_vrst, mat_slope, mat_nan, \
-    mat_a,   \
-    mat_n,   \
-    output, pixel_area, points, poradi,  end_time, spix, state_cell, \
-    temp, type_of_computing, vpix, mfda, sr, itera, \
-    toky, cell_stream, mat_tok_usek, STREAM_RATIO, tokyLoc = prepare_data(sys.argv)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
