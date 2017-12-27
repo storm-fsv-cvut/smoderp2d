810d4d86fcf44b59f9f588cbd4e63b201aa7ff4d
jerabekjak <jerabekjak@email.cz>
2017-12-27 18:38:51 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+setup(name='smoderp2d',
+      version='0.1',
+      packages=find_packages(),
+      description='Simulacni model odtokovych a eroznich proceso',
+      author='Vrana, Kavka',
+      author_email='...@fsv.cvut.cz',
+      url='http://storm.fsv.cvut.cz/cinnost-katedry/volne-stazitelne-vysledky/smoderp/?lang=cz',
+      )
+
+
+
+
+
