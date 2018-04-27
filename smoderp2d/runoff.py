146b5ae2664975f2316e51201e5cdd898cce2ad4
jerabekjak <jerabekjak@fsv.cvut.cz>
2018-04-27 11:19:42 +0200

    move MaxIterError to smoderp2d/exceptions

--- smoderp2d/runoff.py
+++ smoderp2d/runoff.py
@@ -51,26 +51,6 @@ from smoderp2d.tools.tools import get_argv
 
 from smoderp2d.exceptions import MaxIterationExceeded
 
-class Error(Exception):
-
-    """Base class for exceptions in this module."""
-    pass
-
-
-class MaxIterationExceeded(Error):
-
-    """Exception raised if number of iteration exceed max iteration criterion
-
-    Attributes:
-        maxIter  -- max n of iteration
-    """
-
-    def __init__(self, mi, t):
-        self.msg = 'Maximum of iterations (maxIter = ' + str(
-            mi) + ') was exceeded of at time [s]: ' + str(t) + '.'
-
-    def __str__(self):
-        return repr(self.msg)
 
 
 # FlowControl manage variables contains variables
