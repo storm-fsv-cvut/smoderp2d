bf42e21a0b078c78f4e0dc13931d62b402b081c6
Martin Landa <landa.martin@gmail.com>
2018-05-18 09:02:49 +0200

    clean up core.__init__

--- smoderp2d/core/__init__.py
+++
@@ -1,9 +0,0 @@
-all = [
-    'CumulativeMax.py',
-     'Flow.py',
-     'General.py',
-     'KinematicDiffuse.py',
-     'Stream.py',
-     'Subsurface.py',
-     'Surface.py',
-     'Vegetation.py']
