a4ea0dea6049dcc35e791fc45cf590e52588e0ac
jerabekjak <jerabekjak@email.cz>
2018-11-23 15:31:06 +0100

    fix names comment out hydrograph record (temporarly)

--- smoderp2d/time_step.py
+++ smoderp2d/time_step.py
@@ -10,7 +10,7 @@ import copy
 import numpy as np
 
 
-from smoderp2d.core.surface import runoff
+from smoderp2d.core.surface import sheet_runoff
 from smoderp2d.core.surface import surface_retention
 
 infilt_capa = 0
@@ -25,12 +25,20 @@ max_infilt_capa = 0.003  # [m]
 class TimeStep:
     
     def do_sheet_flow(self, surface, subsurface, delta_t, flow_control, courant):
+        
+        global infilt_capa
+        global max_infilt_capa
+        global infilt_time
+        
+        
         rr, rc = GridGlobals.get_region_dim()
         mat_efect_vrst = Globals.get_mat_efect_vrst()
         fc = flow_control
         sr = Globals.get_sr()
         itera = Globals.get_itera()
-
+        combinatIndex = Globals.get_combinatIndex()
+        NoDataValue = GridGlobals.get_no_data()
+        
         # calculate potential rainfall
         potRain, fc.tz = rain_f.timestepRainfall(
             itera, fc.total_time, delta_t, fc.tz, sr)
@@ -40,18 +48,18 @@ class TimeStep:
             infilt_time += delta_t
             actRain = 0.0
             potRain = 0.0
-            for i in rr:
-                for j in rc[i]:
-                    hydrographs.write_hydrographs_record(
-                        i,
-                        j,
-                        flow_control,
-                        courant,
-                        delta_t,
-                        surface,
-                        subsurface,
-                        actRain)
-            return actRain
+            #for i in rr:
+                #for j in rc[i]:
+                    #hydrographs.write_hydrographs_record(
+                        #i,
+                        #j,
+                        #flow_control,
+                        #courant,
+                        #delta_t,
+                        #surface,
+                        #subsurface,
+                        #actRain)
+            return 0
 
         for iii in combinatIndex:
             index = iii[0]
@@ -80,11 +88,13 @@ class TimeStep:
             for j in rc[i]:
                 
                 # sheet water level in previous time step
-                h_sheet_old = surface.arr[i][j].h_sheet_old
+                h_sheet_pre = surface.arr[i][j].h_sheet_pre
                 
                 # actual rainfall 
-                act_rain, fc.sum_interception, rain_arr.arr[i][j].veg_true = rain_f.current_rain(
-                    rain_arr.arr[i][j], potRain, fc.sum_interception)
+                # TODO actual rainfall is still potential rainfall
+                act_rain = potRain
+                #act_rain, fc.sum_interception, rain_arr.arr[i][j].veg_true = rain_f.current_rain(
+                    #rain_arr.arr[i][j], potRain, fc.sum_interception)
                 # store current rain
                 surface.arr[i][j].cur_rain = act_rain
                 
@@ -92,10 +102,10 @@ class TimeStep:
                 inflows = surface.cell_sheet_inflows(i,j,delta_t)
                 
                 # sheet outflow 
-                outflow = surface.sheet_runoff(surface.arr[i][j], delta_t)
+                outflow = sheet_runoff(surface.arr[i][j], delta_t)
                 
                 # calculate surface balance
-                sur_bil = h_sheet_old + actua inflows - outflow
+                sur_bil = h_sheet_pre + act_rain + inflows - outflow
                 
                 # reduce be infiltration
                 sur_bil, infiltration = infilt.philip_infiltration(
