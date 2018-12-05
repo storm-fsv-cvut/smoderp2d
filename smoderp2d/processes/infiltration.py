953010f17859a9ccf85c2b41f0222e2e3ab75b67
jerabekjak <jerabekjak@email.cz>
2018-12-05 14:18:58 +0100

    init in green ampt class

--- smoderp2d/processes/infiltration.py
+++ smoderp2d/processes/infiltration.py
@@ -76,6 +76,13 @@ class GreenAmptInfiltration(BaseInfiltration):
     def __init__(self, combinatIndex):
 
         self._combinat_index = combinatIndex
+        #TODO include into inputs, should be changeable
+        self._psi = -10/100 # soil water potential at the wetting front
+        #TODO include into inputs, should be changeable
+        self._d_theta = 0.4 # theta difference
+        
+        
+    
 
     def precalc(self, dt, total_time):
         raise NotImplemented("Not implemented for Green Ampt infiltration")
