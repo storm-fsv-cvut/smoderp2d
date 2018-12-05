93d2a240aafaa79c5ea141a7014286e75d98dcce
jerabekjak <jerabekjak@email.cz>
2018-12-05 13:36:49 +0100

    add empty class for green ampt and richards infiltration #13

--- smoderp2d/processes/infiltration.py
+++ smoderp2d/processes/infiltration.py
@@ -15,6 +15,7 @@ def set_combinatIndex(newCombinatIndex):
     global combinatIndex
     combinatIndex = newCombinatIndex
 
+
 def philip(k, s, deltaT, totalT, NoDataValue):
     if k and s == NoDataValue:
         infiltration = NoDataValue
@@ -23,21 +24,20 @@ def philip(k, s, deltaT, totalT, NoDataValue):
     return infiltration
 
 
-
 # base infiltration is philips infiltration
 class BaseInfiltration(object):
-    
-    def __init__(self,combinatIndex) :
-        
+
+    def __init__(self, combinatIndex):
+
         self._combinat_index = combinatIndex
-        
-    def precalc(self,dt, total_time) :
+
+    def precalc(self, dt, total_time):
         """ Precalculates potential infiltration got a given soil.
         The precalculated value is storred in the self._combinat_index
         """
-        
+
         NoDataValue = GridGlobals.get_no_data()
-        
+
         for iii in self._combinat_index:
             index = iii[0]
             k = iii[1]
@@ -48,10 +48,10 @@ class BaseInfiltration(object):
                 dt,
                 total_time,
                 NoDataValue)
-            
-    def current_infiltration(self,soil,bil) :
+
+    def current_infiltration(self, soil, bil):
         """ Returns the actual infiltrated water height 
-        
+
         :param soil: soil type in current cell
         :param bil: current water level
         """
@@ -67,5 +67,28 @@ class BaseInfiltration(object):
                     bil = bil - infiltration
         return bil, infiltration
 
-            
-            
+
+class GreenAmptInfiltration(BaseInfiltration):
+
+    def __init__(self, combinatIndex):
+
+        self._combinat_index = combinatIndex
+
+    def precalc(self, dt, total_time):
+        raise NotImplemented("Not implemented for Green Ampt infiltration")
+
+    def current_infiltration(self, soil, bil):
+        raise NotImplemented("Not implemented for Green Ampt infiltration")
+
+
+class RichardsInfiltration(BaseInfiltration):
+
+    def __init__(self, combinatIndex):
+
+        self._combinat_index = combinatIndex
+
+    def precalc(self, dt, total_time):
+        raise NotImplemented("Not implemented for Richards infiltration")
+
+    def current_infiltration(self, soil, bil):
+        raise NotImplemented("Not implemented for Richards infiltration")
