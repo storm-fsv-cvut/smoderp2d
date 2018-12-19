b8541d14b1a85d209310031e9f7f17d99913e740
jerabekjak <jerabekjak@email.cz>
2018-12-19 15:57:45 +0100

    add coments to functions #13

--- smoderp2d/processes/infiltration/greenamptunsteadyrain.py
+++ smoderp2d/processes/infiltration/greenamptunsteadyrain.py
@@ -9,39 +9,106 @@ except ImportError:
 
 
 class GreenAmptInfiltrationUnsteadyRain(BaseInfiltration):
-    """ Green ampt infiltariont for unsteady rainfall
+    """ Green ampt infiltration for unsteady rainfall
 
     based on S. T. Chu., Infiltration During an Unsteady Rain, 
     Water Resources Research,  vol. 14, no. 3, 1978
     """
 
-    def indicator(self, P_n, R_n_1, K, SM, I):
-        return P_n - R_n_1 - K*SM/(I-K)
+    def __init__(self, ks, sm):
+        """ for each soil type sets the variables 
 
-    def time_ponding(self, P_n_1, R_n_1, K, SM, I, t_n_1):
-        return (K*SM/(I-K) - P_n_1 + R_n_1)/I + t_n_1
+        ks - saturated hydraulic conductivity
+        sm - suction pressure at the wetting front (s) time difference of the moisture (m)
+        """
 
-    def pseudo_time(self, Pt_p, R_n_1, K, SM, I):
-        return ((Pt_p - R_n_1)/SM - math.log(1+(Pt_p - R_n_1)/SM))*SM/K
+        self._ks = ks
+        self._sm = sm
 
-    def _greenampt_F(self, Fp, K, T, SM):
+        # cumulative rainfall
+        self._p_n_1 = self._p_n = 0.0
+        # cumulative runoff
+        self._r_n_1 = self._r_n = 0.0
+        # cumulative infiltration
+        self._f_n_1 = self._f_n = 0.0
 
-        return K*T/SM - (Fp/SM - math.log(1+Fp/SM))
+    def _indicator(self, p_n, r_n_1, ks, sm, i):
+        """ indicating the ponding if the value is larger thant zero 
 
-    def _cumulative_F(self, Fp, K, T, SM):
+        C_u in Chu (1978)
 
-        def FFp(Fp): return _greenampt_F(Fp, K, T, SM)
+        :param p_n: current cumulative precipitation
+        :param r_n_1: previous cumulative runoff 
+        :param ks: saturated hydraulic conductivity
+        :param sm: suction pressure at the wetting front (s) time difference of the moisture (m)
+        :param i: current precipitation intenzity
+        """
 
-        return newton(FFp, K*T)
+        return p_n - r_n_1 - k*sm/(i-k)
 
-    def main(self, sr):
+    def _time_ponding(self, p_n_1, r_n_1, ks, sm, i, t_n_1):
+        """ returns time of ponding in current time step
 
-        K = 0.0142  # m/h
-        SM = 0.036  # m
+        t_p in Chu (1978)
 
-        P_n_1 = P_n = 0.0
-        R_n_1 = R_n = 0.0
-        F_n_1 = F_n = 0.0
+        :param p_n_1: previous cumulative precipitation
+        :param r_n_1: previous cumulative runoff 
+        :param ks: saturated hydraulic conductivity
+        :param sm: suction pressure at the wetting front (s) time difference of the moisture (m)
+        :param i: current precipitation intenzity
+        :param t_n_1: previous time
+        """
+
+        return (ks*sm/(i-ks) - p_n_1 + r_n_1)/i + t_n_1
+
+    def _pseudo_time(self, pt_p, r_n_1, k, sm, i):
+        """ calculates preudo time 
+
+        t_s in Chu (1978)
+
+        :param p_n_1: precipitation at ponding time 
+        :param r_n_1: previous cumulative runoff 
+        :param ks: saturated hydraulic conductivity
+        :param sm: suction pressure at the wetting front (s) time difference of the moisture (m)
+        :param i: current precipitation intenzity
+        :param t_n_1: previous time
+        """
+
+        return ((pt_p - r_n_1)/sm - math.log(1+(pt_p - r_n_1)/sm))*sm/k
+
+    def _greenampt_f(self, fp, ks, t, sm):
+        """ green ampt formula 
+
+
+        :param fp: cumulative infiltration
+        :param ks: saturated hydraulic conductivity
+        :param t: time 
+        :param sm: suction pressure at the wetting front (s) time difference of the moisture (m)
+        """
+
+        return ks*t/sm - (fp/sm - math.log(1+fp/sm))
+
+    def _cumulative_f(self, fp, ks, t, sm):
+        """ Solve current cumulative infiltration ne newton's mehod 
+
+        :param fp: cumulative infiltration
+        :param ks: saturated hydraulic conductivity
+        :param t: time 
+        :param sm: suction pressure at the wetting front (s) time difference of the moisture (m)
+        """
+
+        # set variebls into green-ampt
+        def ffp(fp): return _greenampt_f(fp, ks, t, sm)
+
+        # compute current cumulative infiltration
+        return newton(ffp, ks*t)
+
+    def calc_next(self, sr, t, dt):
+        """ calculates next cumulative infiltration for a single soil type
+        :param sr: current rainfall height 
+        :param t: current time 
+        :param dt: current time step
+        """
 
         ponded = False
 
@@ -75,15 +142,13 @@ class GreenAmptInfiltrationUnsteadyRain(BaseInfiltration):
 
             P_n_1 = P_n
 
-            print P_n, I, F_n, I
-
         return 1
 
 
 if __name__ == "__main__":
-    
+
     sr = [[0., 0.], [7.167, 0.0206], [7.333, 0.0212], [7.417, 0.0244], [
         7.583, 0.0270], [7.667, 0.0308], [7.917, 0.0313], [8.000, 0.0346]]
     #sr = [[0.,0.],[0.083,0.0013],[0.667,0.0013],[0.917,0.0216],[1.167,0.0221]]
-    
+
     t = GreenAmptInfiltrationUnsteadyRain([[0, 2.777e-1, 2, 0]])
