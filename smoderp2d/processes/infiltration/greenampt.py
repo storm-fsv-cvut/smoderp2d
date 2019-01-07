ad10055bc5debecf2ba27662e0323b99b9a87e96
jerabekjak <jerabekjak@email.cz>
2019-01-07 16:06:21 +0100

    green ampt works

--- smoderp2d/processes/infiltration/greenampt.py
+++ smoderp2d/processes/infiltration/greenampt.py
@@ -4,68 +4,86 @@ from scipy.optimize import newton
 
 import math
 
+class SingleSoilGA(object):
+    """ Green ampt infiltration for single soil type """
+    
+    def __init__(self, ks):
+        """ for each soil type sets the variables 
 
-class GreenAmptInfiltration(BaseInfiltration):
+        ks - saturated hydraulic conductivity
+        sm - suction pressure at the wetting front (s) time difference of the moisture (m)
+        """
 
-    def __init__(self, combinatIndex):
+        self._ks = ks
 
-        self._combinat_index = combinatIndex
         # TODO include into inputs should be different for different soils
-        self._psi = 10./100.  # soil water potential at the wetting front
+        self._psi = 0.1  # soil water potential at the wetting front
         # TODO include into inputs should be different for different soils
         self._d_theta = 0.4  # theta difference
+        
+        # only flobal variable
+        # stores the infiltration height
+        # for a given time step
+        self.infiltration = 0.0
 
-    def _greenampt_F(self, F, k, t):
+    def _greenampt_F(self, F, t):
         """ Green-Ampt formula for cumulative infiltration 
 
         solution if found with Newton's method in optimization in _cumulative_F
 
         :param F: cumulative infiltration
-        :param k: hydraulic conductivity
         :param t: t
         """
-        return F - k*t + self._psi * self._d_theta * math.log(1 + F/(self._psi * self._d_theta))
+        return F - self._ks*t + self._psi * self._d_theta * math.log(1 + F/(self._psi * self._d_theta))
 
-    def _cumulative_F(self, k, t):
+    def _cumulative_F(self, t):
         """ Computes cumulative infiltration with Newton's method
 
-        :param k: hydraulic conductivity
         :param t: t
 
         :return : cumulative infiltration in a given time
         """
 
-        def FF(F): return self._greenampt_F(F, k, t)
+        def FF(F): return self._greenampt_F(F, t)
 
-        return newton(FF, k*t)
+        return newton(FF, self._ks*t)
 
-    def _greenampt_f(self, k, t, dt, NoDataValue):
+    def greenampt_f(self, t, dt):
         """ Computes infiltration rate 
 
-        :param k: hydraulic conductivity
         :param t: time
         :param dt: time step
+        """
+        NoDataValue = GridGlobals.get_no_data()
+        if (t == 0.0):
+            self.infiltration = 1e108
+        else:
+            if self._ks == NoDataValue:
+                self.infiltration = NoDataValue
+            else:
+                F_t = self._cumulative_F(t)
+                self.infiltration = self._ks*((self._psi * self._d_theta)/F_t + 1)
+                
 
-        :return : infiltration rate multiplied with time step
+class GreenAmptInfiltration(BaseInfiltration):
+
+    def __init__(self, soils_data):
+        """ make instance of SingleSoilGA for each soil type 
+
+        :param soils_data: combinat_index in the smoderp2d code
         """
 
-        if k == NoDataValue:
-            return NoDataValue
-        else:
-            F_t = self._cumulative_F(k, t)
-            return k*((self._psi * self._d_theta)/F_t + 1)*dt
+        self._n = len(soils_data)
+        self._soil = []
+        for i in range(self._n):
+            self._soil.append(SingleSoilGA(
+                ks=soils_data[i][1]))
+
 
     def precalc(self, dt, total_time):
         """ Precalculates potential infiltration got a given soil.
-        The precalculated value is storred in the self._combinat_index
-        """
+        The precalculated value is storred in the self._combinat_index """
+        
+        for i in range(self._n):
+            self._soil[i].greenampt_f(total_time, dt)
 
-        NoDataValue = GridGlobals.get_no_data()
-
-        for iii in self._combinat_index:
-            k = iii[1]
-            iii[3] = self._greenampt_f(
-                k,
-                dt,
-                total_time,
-                NoDataValue)
