eb9246d5d447afa6464c7caf742490d8be0372dc
Martin Landa <landa.martin@gmail.com>
2019-01-10 10:47:11 +0100

    avoid smoderp2d.main

--- bin/start-smoderp2d.py
+++ bin/start-smoderp2d.py
@@ -5,8 +5,12 @@ import sys
 
 if __name__ == "__main__":
     sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
-    import smoderp2d.main as sm
+    from smoderp2d import run
+    from smoderp2d.exceptions import ProviderError
 
-    sm.run()
+    try:
+        sys.exit(run())
+    except ProviderError as e:
+        sys.exit(e)
 else:
     sys.exit("Can be run only as standalone program.")
--- smoderp2d/__init__.py
+++ smoderp2d/__init__.py
@@ -20,3 +20,33 @@ The computational options are as follows:
    - yes
    - no
 """
+
+import os
+
+def run():
+    # initialize provider
+    if os.getenv('ESRIACTIVEINSTALLATION'):
+        from smoderp2d.providers.arcgis import ArcGisProvider
+        provider_class = ArcGisProvider
+    elif os.getenv('GISRC'):
+        from smoderp2d.providers.grass import GrassProvider
+        provider_class = GrassProvider
+    else:
+        from smoderp2d.providers.cmd import CmdProvider
+        provider_class = CmdProvider
+    provider = provider_class()
+
+    # print logo
+    provider.logo()
+
+    # load configuration (set global variables)
+    provider.load()
+
+    # must be called after initialization (!)
+    from smoderp2d.runoff import Runoff
+
+    # the computation
+    runoff = Runoff(provider)
+    runoff.run()
+
+    return 0
