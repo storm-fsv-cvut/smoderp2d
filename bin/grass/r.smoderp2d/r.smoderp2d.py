7b9d026186abb92d7ef01e7e567a104f6c60561e
Martin Landa <landa.martin@gmail.com>
2019-01-10 11:34:56 +0100

    r.smoderp: do not define default answers

--- bin/grass/r.smoderp2d/r.smoderp2d.py
+++ bin/grass/r.smoderp2d/r.smoderp2d.py
@@ -23,46 +23,40 @@
 #% keyword: erosion
 #%end
 #%option G_OPT_R_ELEV
-#% description:  Input surface raster
-#% answer: w001001
+#% description: Input surface raster
 #%end
 #%option G_OPT_V_INPUT
 #% key: soil
-#% description:  Input soil polygon features
-#% answer: puda
+#% description: Input soil polygon features
 #%end
 #%option G_OPT_DB_COLUMN
 #% key: soil_type
 #% description: Soil types
-#% answer: Novak
 #% required: yes
 #%end
 #%option G_OPT_V_INPUT
 #% key: vegetation
-#% description:  Input land use polygon features
-#% answer: puda
+#% description: Input land use polygon features
 #%end
 #%option
 #% key: vegetation_type
 #% description: Land use types
-#% answer: vegetace
 #% required: yes
 #%end
 #%option G_OPT_F_INPUT
 #% key: rainfall_file
 #% description: Rainfall file
-#% answer: ../../../tests/data/srazka.txt
 #%end
 #%option
 #% key: maxdt
-#% type: float
+#% type: double
 #% description: Max time step [sec]
 #% answer: 30
 #% required: yes
 #%end
 #%option
 #% key: end_time
-#% type: float
+#% type: double
 #% description: Total running time [min]
 #% answer: 5
 #% required: yes
@@ -70,46 +64,41 @@
 #%option G_OPT_V_INPUT
 #% key: points
 #% description:  Input points features
-#% answer: points2
 #% required: no
 #%end
 #%option G_OPT_M_DIR
 #% key: output
 #% description: Output folder
-#% answer: ../../../tests/data/output
+#% required: yes
 #%end
 #%option G_OPT_DB_TABLE
 #% key: table_soil_vegetation
 #% description: Table of soil and land use information
-#% answer: tabulkytab
 #%end
 #%option G_OPT_DB_COLUMN
-#% key: table_soil_vegetation
+#% key: table_soil_vegetation_code
 #% description: Soil land use code
-#% answer: SOILVEG
 #%end
 #%option G_OPT_V_INPUT
 #% key: streams
 #% description:  Reach feture
-#% answer: tok
 #% required: no
 #%end
 #%option G_OPT_DB_TABLE
 #% key: table_stream_shape
 #% description: Reach shapes table
-#% answer: tab_stream_tvar
 #%end
 #%option G_OPT_DB_COLUMN
 #% key: table_stream_shape_code
 #% description: Reach shape table code
-#% answer: smoderp
 #%end
 
-
+import os
 import sys
 import grass.script as gs
 
-from smoderp2d.smoderp2d.main import run
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', '..'))
+from smoderp2d import run
 from smoderp2d.exceptions import ProviderError
 
 if __name__ == "__main__":
