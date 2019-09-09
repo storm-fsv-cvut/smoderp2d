b8f9f51a246f1c6006ed3def0b000a84fde649ca
jerabekjak <jerabekjaku@gmail.com>
2019-09-09 16:05:27 +0200

    fix varianle naming based on 8db0da85cad2c6c851bc5a2dad28544bcd07dd0c, 7af8b611d123bf3d99778eb5400b86e295a768be and 00c4fdad426a26049e2119acdf688aa170266155 plus update_cumulative method header fix and documentation

--- smoderp2d/core/cumulative_max.py
+++ smoderp2d/core/cumulative_max.py
@@ -110,102 +110,83 @@ class Cumulative(GridGlobals, CumulativeSubsurface if Globals.subflow else Cumul
         Logger.info('Save cumulative and maximum values from: Surface')
 
         # Dictionary stores the python arrays identification.
-        self.arrs = {'infiltration' : ('core', 'cinfil_m'),         # 1
-                     'precipitation': ('core', 'crainf_m'),         # 2
-                     'h_sur'        : ('core', 'cVInM3'),           # 3
-                     'q_sur'        : ('core', 'MaxQL3t_1'),        # 4
-                     'v_sur'        : ('core', 'cSheetVOutM3'),     # 5
-                     'v_sur2'       : ('core', 'mvel_m_s'),         # 6
-                     'shear_sur'    : ('core', 'mshearstr_pa'),     # 7
-                     'h_rill'       : ('core', 'MaxWaterRillL'),    # 8
-                     'q_rill'       : ('core', 'MaxQRillL3t_1'),    # 9
-                     'v_rill'       : ('core', 'cRillVOutL3'),      # 10
-                     'b_rill'       : ('core', 'AreaRill'),         # 11
-                     'inflow_sur'   : ('core', 'CumVInL3'),         # 12
-                     'sur_ret'      : ('core', 'SurRet'),           # 13
-                     'v_sur_r'      : ('core', 'CumVRestL3'),       # 14
-                     'q_sur_tot'    : ('core', 'msurfl_m3_s'),      # 15
-                     'v_sur_tot'    : ('core', 'csurvout_m3_s')     # 16
+        self.arrs = {'infiltration' : ('core', 'cInfil_m3'),       # 1
+                     'precipitation': ('core', 'cRain_m3'),        # 2
+                     'h_sur_tot'    : ('control', 'mWLevel_m'),    # 3
+                     'q_sheet'      : ('control', 'mQsheet_m3_s'), # 4
+                     'vol_sheet'    : ('control', 'cSheetVOutM3'), # 5
+                     'v_sheet'      : ('control', 'mVel_m_s'),     # 6
+                     'shear_sheet'  : ('control', 'mrSearStr_Pa'), # 7
+                     'h_rill'       : ('control', 'mWLevelRill_m'),# 8
+                     'q_rill'       : ('control', 'mQRill_m3_s'),  # 9
+                     'vol_rill'     : ('control', 'cRillVOut_m3'), # 10
+                     'b_rill'       : ('control', 'widthRill'),    # 11
+                     'inflow_sur'   : ('control', 'cVIn_M3'),      # 12
+                     'sur_ret'      : ('control', 'surRet_M'),     # 13
+                     'vol_sur_r'    : ('control', 'CumVRestL3'),   # 14 not being used 
+                     'q_sur_tot'    : ('core', 'mQsur_m3_s'),      # 15
+                     'vol_sur_tot'  : ('core', 'cVsur_m3')         # 16
         }
 
-        # array count stored in the class
-        self.n = 13
-        # cumulative infiltrated volume [m3]
-        self.infiltration = np.zeros([self.r, self.c], float)
-        # cumulative precipitation volume [m3]
-        self.precipitation = np.zeros([self.r, self.c], float)
-        # maximum surface water level [m]
-        self.h_sur_tot = np.zeros([self.r, self.c], float)
-        # maximum sheet discharge [m3s-1]
-        self.q_sheet = np.zeros([self.r, self.c], float)
-        # cumulative sheet runoff volume [m3]
-        self.vol_sheet = np.zeros([self.r, self.c], float)
-        # cumulative surface runoff volume [m3]
-        self.v_sur_r = np.zeros([self.r, self.c], float) # asi se nepouziva
-        # maximum sheet velocity [ms-1]
-        self.v_sheet = np.zeros([self.r, self.c], float)
-        # maximum sheet shear stress [Pa]
-        self.shear_sheet = np.zeros([self.r, self.c], float)
-        # cumulative surface inflow volume [m3]
-        self.inflow_sur = np.zeros([self.r, self.c], float)
-        # maximum water level in rills [m]
-        self.h_rill = np.zeros([self.r, self.c], float)
-        # maximum discharge in rills [m3s-1]
-        self.q_rill = np.zeros([self.r, self.c], float)
-        # cumulative runoff volume in rills [m3]
-        self.vol_rill = np.zeros([self.r, self.c], float)
-        # cumulative runoff volume in rills [m3]
-        #self.v_rill_r = np.zeros([self.r, self.c], float)
-        # maximum rill width [m]
-        self.b_rill = np.zeros([self.r, self.c], float)
-        # maximum velocity in rills [ms-1]
-        self.v_rill = np.zeros([self.r, self.c], float)
-        # maximum surface retention [m]
-        self.sur_ret = np.zeros([self.r, self.c], float)
-        # maximal total surface flow [m3/s]
-        self.q_sur_tot = np.zeros([self.r, self.c], float)
-        # cumulative total surface flow [m3/s]
-        self.vol_sur_tot = np.zeros([self.r, self.c], float)
+        # arrays which stores variables defined in self.arrs is defined
+        # based on the dictionary keys self.arrs
+        for item in self.arrs.keys():
+            setattr(self,item,
+                    np.zeros([self.r, self.c], float))
+
 
     # Method is used after each time step to save the desired variables.
     #
     #  Method is called in smoderp2d.runoff
     #
-    def update_cumulative(self, i, j, surface, subsurface, delta_t):
+    def update_cumulative(self, i, j, sur_arr_el, subsur_arr_el, delta_t):
+        """ Update arrays with cumulative and maximum 
+        values of key computation results.
+        
+        :param int i:
+        :param int j:
+        :param float sur_arr_el: single element in surface.arr
+        :param float subsur_arr_el: single element in subsurface.arr
+        :param floet delta_t: length of time step
+        """
 
-        self.infiltration[i][j] += surface.infiltration * self.pixel_area
-        self.precipitation[i][j] += surface.cur_rain * self.pixel_area
-        self.vol_sheet[i][j] += surface.vol_runoff
-        #self.v_sur_r[i][j] += surface.vol_rest
-        self.vol_sur_tot[i][j] += surface.vol_to_rill + surface.vol_runoff
-        self.inflow_sur[i][j] += surface.inflow_tm
-        self.sur_ret[i][j] += surface.cur_sur_ret * self.pixel_area
+        self.infiltration[i][j] += sur_arr_el.infiltration * self.pixel_area
+        self.precipitation[i][j] += sur_arr_el.cur_rain * self.pixel_area
+        self.vol_sheet[i][j] += sur_arr_el.vol_runoff
+        #self.v_sur_r[i][j] += sur_arr_el.vol_rest
+        self.vol_sur_tot[i][j] += sur_arr_el.vol_runoff_rill + sur_arr_el.vol_runoff
+        self.inflow_sur[i][j] += sur_arr_el.inflow_tm
+        self.sur_ret[i][j] += sur_arr_el.cur_sur_ret * self.pixel_area
 
-        q_sheet = surface.vol_runoff / delta_t
-        q_rill = surface.vol_runoff_rill / delta_t
+        print (sur_arr_el.vol_runoff)
+        print (self.vol_sheet[3][3])
+
+
+        q_sheet = sur_arr_el.vol_runoff / delta_t
+        q_rill = sur_arr_el.vol_runoff_rill / delta_t
         q_tot = q_sheet + q_rill
         if q_tot > self.q_sur_tot[i][j]:
             self.q_sur_tot[i][j] = q_tot
 
-        if surface.state == 0:
-            if surface.h_total_new > self.h_sur_tot[i][j]:
-                self.h_sur_tot[i][j] = surface.h_total_new
+        if sur_arr_el.state == 0:
+            if sur_arr_el.h_total_new > self.h_sur_tot[i][j]:
+                self.h_sur_tot[i][j] = sur_arr_el.h_total_new
                 self.q_sheet[i][j] = q_sheet
 
-        elif (surface.state == 1) or (surface.state == 2):
-            self.vol_rill[i][j] += surface.vol_runoff_rill
-            #self.v_rill_r[i][j] += surface.v_rill_rest
-            if surface.h_total_new > self.h_sur_tot[i][j]:
-                self.h_sur_tot[i][j] = surface.h_total_new
+        elif (sur_arr_el.state == 1) or (sur_arr_el.state == 2):
+            self.vol_rill[i][j] += sur_arr_el.vol_runoff_rill
+            #self.v_rill_r[i][j] += sur_arr_el.v_rill_rest
+            if sur_arr_el.h_total_new > self.h_sur_tot[i][j]:
+                self.h_sur_tot[i][j] = sur_arr_el.h_total_new
                 self.q_sheet[i][j] = q_sheet
 
-            elif surface.h_rill > self.h_rill[i][j]:
-                self.h_rill[i][j] = surface.h_rill
-                self.b_rill[i][j] = surface.rillWidth
+            elif sur_arr_el.h_rill > self.h_rill[i][j]:
+                self.h_rill[i][j] = sur_arr_el.h_rill
+                self.b_rill[i][j] = sur_arr_el.rillWidth
                 self.q_rill[i][j] = q_rill
 
         self.update_cumulative_sur(
             i, j,
-            subsurface.arr[i][j],
-            subsurface.q_subsurface
+            subsur_arr_el,
+            subsur_arr_el
         )
