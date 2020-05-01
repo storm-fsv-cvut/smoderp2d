8ae91ef4cc25543d218c67876240a3da335b36b0
Martin Landa <landa.martin@gmail.com>
2020-05-01 12:27:48 +0200

    grass python test: wait for results

--- bin/grass/test_r_smoderp2d.py
+++ bin/grass/test_r_smoderp2d.py
@@ -7,7 +7,7 @@ import subprocess
 test_dir = os.path.dirname(__file__)
 tmp_dir = tempfile.mkdtemp()
 
-subprocess.Popen([
+test = subprocess.Popen([
     os.environ['GRASS_PYTHON'],
     os.path.join(test_dir, 'r.smoderp2d', 'r.smoderp2d.py'),
     'elevation=dem10m',
@@ -26,3 +26,4 @@ subprocess.Popen([
     'end_time=120',
     'output_dir={}_{}'.format(os.path.join(tmp_dir, 'smoderp2d'), os.getpid())
 ])
+test.wait()
