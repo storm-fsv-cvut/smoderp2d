ecb62148571ada75a31e31d6dd20e45e6c82afe2
Martin Landa <landa.martin@gmail.com>
2020-11-06 17:39:23 +0100

    wps nginx, add letsencrypt

--- bin/wps/startup.sh
+++ bin/wps/startup.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-envsubst '$NGINX_HOST' < /etc/nginx/conf.d/smoderp.template > \
-         /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;' &
+envsubst '$NGINX_HOST $NGINX_PORT' < /opt/pywps/pywps.cfg.template > \
+         /opt/pywps/pywps.cfg
+
+envsubst '$NGINX_HOST' < /etc/nginx/conf.d/default.conf.template > \
+         /etc/nginx/conf.d/default.conf
+if test -f /etc/nginx/conf.d/conf.letsencrypt.template; then
+    envsubst '$NGINX_HOST' < /etc/nginx/conf.d/conf.letsencrypt.template > \
+             /etc/nginx/conf.d/conf.letsencrypt
+    envsubst '$NGINX_HOST' < /etc/nginx/conf.d/ssl-parameters.template > \
+             /etc/nginx/conf.d/ssl-parameters
+    sed -i 's/http/https/g' /opt/pywps/pywps.cfg
+fi
+nginx -g 'daemon off;' &
+
 gunicorn3 -b 127.0.0.1:8081 --workers $((2*`nproc --all`)) \
           --log-syslog  --pythonpath /opt/pywps pywps_app:application
 
