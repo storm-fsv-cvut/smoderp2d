86064d578fe6db6dc75f1ff6c77787429a8c3df4
pesekon2 <pesej.ondrek@gmail.com>
2020-12-20 11:51:05 +0100

    perform set_outsides() in Vegetation

--- smoderp2d/core/vegetation.py
+++ smoderp2d/core/vegetation.py
@@ -3,6 +3,7 @@
 import numpy as np
 
 from smoderp2d.core.general import GridGlobals, DataGlobals, Globals
+from smoderp2d.core.surface import SurArrs
 
 class VegArrs(object):
     def __init__(self, veg, ppl, pi):
@@ -20,6 +21,8 @@ class Vegetation(GridGlobals):
     def __init__(self):
         """Class stores info about the vegetation cover."""
         super(Vegetation, self).__init__()
+
+        self.arr.set_outsides(SurArrs)
 
         # convert unit mm -> m
         # TODO move this conversion into data preparation
