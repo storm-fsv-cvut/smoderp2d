26dbd54b2748136250bbadbf561b44a19ae276da
Martin Landa <landa.martin@gmail.com>
2022-10-04 12:25:39 +0200

    remove outdir before run

--- tests/test_cmd.py
+++ tests/test_cmd.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import configparser
+from shutil import rmtree
 from pathlib import Path
 import pytest
 
@@ -7,8 +9,6 @@ class TestCmd:
     config_file = Path(__file__).parent / "quicktest.ini"
 
     def test_001_read_config(self):
-        import configparser
-
         assert Path(self.config_file).exists()
 
         config = configparser.ConfigParser()
@@ -17,10 +17,15 @@ class TestCmd:
 
     def test_002_run(self):
         sys.path.insert(0, str(Path(__file__).parent.parent / "bin"))
-
         from start import start_smoderp2d
+
+        config = configparser.ConfigParser()
+        config.read(self.config_file)
+        output_path = Path(config["output"]["outdir"])
+        if output_path.exists():
+            rmtree(output_path)
+
         os.environ["SMODERP2D_CONFIG_FILE"] = str(self.config_file)
         start_smoderp2d()
 
-        output_path = Path(__file__).parent / "data" / "output"
-        print(output_path)
+        assert output_path.is_dir()
