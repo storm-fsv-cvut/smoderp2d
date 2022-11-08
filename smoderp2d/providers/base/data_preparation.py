a56f59887eca8793d3ad9c3ef71e8dff3ac21d75
jerabekjak <jerabekjaku@gmail.com>
2022-11-08 16:57:24 +0100

    add mesage that values were checked

--- smoderp2d/providers/base/data_preparation.py
+++ smoderp2d/providers/base/data_preparation.py
@@ -214,6 +214,9 @@ class PrepareDataBase(object):
                 if (range_[0] > val) : raise SmallParameterValue(name, val, range_[0])
                 if (range_[1] < val) : raise LargeParameterValue(name, val, range_[1])
 
+        Logger.info('{} parameters values checked.'.format(name))
+
+
     def _set_output_data(self):
         """
         Creates dictionary to which model parameters are computed.
