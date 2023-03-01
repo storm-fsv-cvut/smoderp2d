1b552416a8113677d725809eb00ac5cfe5f5effa
ondrej <pesej.ondrek@gmail.com>
2023-03-02 00:32:51 +0100

    cumulative_max: cleaning

--- smoderp2d/core/cumulative_max.py
+++ smoderp2d/core/cumulative_max.py
@@ -204,60 +204,6 @@ class Cumulative(CumulativeSubsurface if Globals.subflow else CumulativeSubsurfa
             self.h_sheet_tot
         )
 
-#         #####################
-#
-#         cond = tf.equal(surface.arr.state, 0)
-#         cond_in_true = surface.h_total_new > self.h_sur
-#         cond_in_false_1 = tf.equal(surface.arr.state, 1)
-#         cond_in_false_2 = tf.equal(surface.arr.state, 2)
-#         cond_in_false = tf.cast(tf.cast(cond_in_false_1, tf.int8) +
-#                                 tf.cast(cond_in_false_2, tf.int8),
-#                                 tf.bool)
-#         cond_in_false_true = surface.h_total_new > self.h_sur
-#         cond_in_false_false = surface.h_rill > self.h_rill
-#
-#         h_sur_in_true = tf.where(cond_in_true, surface.h_total_new, self.h_sur)
-#         h_sur_in_false = tf.where(cond_in_false_true,
-#                                   surface.h_total_new, self.h_sur)
-#         h_sur_in_false = tf.where(cond_in_false, h_sur_in_false, self.h_sur)
-#         self.h_sur = tf.where(cond, h_sur_in_true, h_sur_in_false)
-#
-#         q_sur_in_true = tf.where(cond_in_true, q_sheet, self.q_sur)
-#         q_sur_in_false = tf.where(cond_in_false_true, q_sheet, self.q_sur)
-#         q_sur_in_false = tf.where(cond_in_false, q_sur_in_false, self.q_sur)
-#         self.q_sur = tf.where(cond, q_sur_in_true, q_sur_in_false)
-#
-#         self.v_rill = tf.where(cond_in_false, surface.vol_runoff_rill, self.v_rill)
-#         self.v_rill_r = tf.where(cond_in_false,
-#                                  surface.v_rill_rest, self.v_rill_r)
-#
-#         q_rill_in_false_false = tf.where(cond_in_false_false,
-#                                          q_rill, self.q_rill)
-#         q_rill_in_false_true = tf.where(cond_in_false_true,
-#                                         self.q_rill, q_rill_in_false_false)
-#         q_rill_in_false = tf.where(cond_in_false,
-#                                    q_rill_in_false_true, self.q_rill)
-#         self.q_rill = tf.where(cond, self.q_rill, q_rill_in_false)
-#
-#         h_rill_in_false_false = tf.where(cond_in_false_false,
-#                                          surface.h_rill, self.h_rill)
-#         h_rill_in_false_true = tf.where(cond_in_false_true,
-#                                         self.h_rill, h_rill_in_false_false)
-#         h_rill_in_false = tf.where(cond_in_false,
-#                                    h_rill_in_false_true, self.h_rill)
-#         self.h_rill = tf.where(cond, self.h_rill, h_rill_in_false)
-#
-#         b_rill_in_false_false = tf.where(cond_in_false_false,
-#                                          surface.rillWidth, self.b_rill)
-#         b_rill_in_false_true = tf.where(cond_in_false_true,
-#                                         self.b_rill, b_rill_in_false_false)
-#         b_rill_in_false = tf.where(cond_in_false,
-#                                    b_rill_in_false_true, self.b_rill)
-#         self.b_rill = tf.where(cond, self.b_rill, b_rill_in_false)
-#
-#         # TODO TF: Really?
-#         i = j = 0
-# >>>>>>> 095f262 (surface in multiple 2-D arrays instead of one 3-D)
         self.update_cumulative_sur(subsurface)
 
     def calculate_vsheet_sheerstress(self):
