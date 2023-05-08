c716259810d1899e97dc0c9687d0706d3cb02917
ondrej <pesej.ondrek@gmail.com>
2023-05-08 11:33:26 +0200

    rainfall: fix current_rain not being computed for non-standard matrices

--- smoderp2d/processes/rainfall.py
+++ smoderp2d/processes/rainfall.py
@@ -174,22 +174,29 @@ def current_rain(rain, rainfallm, sum_interception):
     rain_ppl = rain.ppl
     rain_pi = rain.pi
     sum_interception_pre = ma.copy(sum_interception)
-    if not ma.all(rain_veg):
-        interc = rain_ppl * rainfallm  # interception is konstant
-        sum_interception += interc  # sum of intercepcion
 
-        NS = ma.where(
+    interc = rain_ppl * rainfallm  # interception is constant
+
+    sum_interception += ma.where(
+        ma.logical_not(rain_veg),
+        interc,
+        0
+    )
+    NS = ma.where(
+        ma.logical_not(rain_veg),
+        ma.where(
             sum_interception >= rain_pi,
-            rainfallm - (rain_pi - sum_interception_pre),  # rest of intercetpion, netto rainfallm
+            rainfallm - (rain_pi - sum_interception_pre),
+            # rest of intercetpion, netto rainfallm
             rainfallm - interc  # netto rainfallm
-        )
-        rain_veg = ma.where(
-            sum_interception >= rain_pi,
-            True,  # as vegetatio interception is full
-            rain_veg
-        )
-    else:
-        NS = rainfallm
+        ),
+        rainfallm
+    )
+    rain_veg = ma.where(
+        ma.logical_and(ma.logical_not(rain_veg), sum_interception >= rain_pi),
+        True,  # as vegetatio interception is full
+        rain_veg
+    )
 
     if isinstance(NS, int):
         pass
