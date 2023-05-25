92db677fbeeb793f3c69e63b9b95a1883ab1c9a0
ondrej <pesej.ondrek@gmail.com>
2023-05-25 16:04:47 +0200

    fix np arrays written to strings instead if single values

--- smoderp2d/io_functions/hydrographs.py
+++ smoderp2d/io_functions/hydrographs.py
@@ -174,7 +174,7 @@ class Hydrographs:
                 m = self.point_int[ip][2]
                 self.files[ip].writelines(
                     '{0:.4e}{sep}{1:.4e}{sep}{2:.4e}{sep}{3}{linesep}'.format(
-                    total_time[l, m], dt, currRain,
+                    total_time[l, m], dt[l, m], currRain[l, m],
                     surface.return_stream_str_vals(l, m, SEP, dt, Globals.extraOut),
                     sep=sep, linesep=os.linesep
                 ))
