70e50bc3447526f486d9db011e7c5aad0a44fc22
Ondrej Pesek <pesej.ondrek@gmail.com>
2023-06-15 17:21:06 +0200

    QGIS provider: make data preparation work (#191)

--- smoderp2d/__init__.py
+++ smoderp2d/__init__.py
@@ -168,10 +168,10 @@ class QGISRunner(GrassGisRunner):
                 if key == "elevation":
                     Module("r.import", input=options[key], output=key)
                 # import vectors
-                elif key in ["soil", "vegetation", "points", "stream"]:
+                elif key in ["soil", "vegetation", "points", "streams"]:
                     Module("v.import", input=options[key], output=key, flags = 'o')
                 # import tables
-                elif key in ["table_soil_vegetation", "table_stream_shape"]:
+                elif key in ["table_soil_vegetation", "channel_properties_table"]:
                     Module("db.in.ogr", input=options[key], output=key)
             except SmoderpError as e:
                 raise SmoderpError('{}'.format(e))
