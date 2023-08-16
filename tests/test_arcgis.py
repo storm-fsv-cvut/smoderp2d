5e77202f80521e3a757b791c906140623a07c03e
ondrej <pesej.ondrek@gmail.com>
2023-08-16 15:42:22 +0200

    test_arcgis: code cleaning

--- tests/test_arcgis.py
+++ tests/test_arcgis.py
@@ -3,6 +3,7 @@ import os
 from smoderp2d import ArcGisRunner
 from dpre_utils import perform_dpre_ref_test, data_dir, output_dir
 
+
 def dpre_params():
     return {
         # parameter indexes from the bin/arcgis/SMODERP2D.pyt tool for ArcGIS
@@ -15,13 +16,16 @@ def dpre_params():
         'maxdt': 30,
         'end_time': 40,
         'points': os.path.join(data_dir, "points.shp"),
-        'table_soil_vegetation': os.path.join(data_dir, "soil_veg_tab_mean.dbf"),
+        'table_soil_vegetation': os.path.join(
+            data_dir, "soil_veg_tab_mean.dbf"
+        ),
         'table_soil_vegetation_fieldname': "soilveg",
         'streams': os.path.join(data_dir, "stream.shp"),
         'channel_properties_table': os.path.join(data_dir, "stream_shape.dbf"),
         'streams_channel_type_fieldname': "channel_id",
         'output': output_dir,
     }
+
 
 if __name__ == "__main__":
     perform_dpre_ref_test(ArcGisRunner, dpre_params, dataprep_only=True)
