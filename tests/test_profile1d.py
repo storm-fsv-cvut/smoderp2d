8b030438d6c0ecd63b3a80ad37b55fee33382051
ondrej <pesej.ondrek@gmail.com>
2023-08-16 15:50:45 +0200

    test_profile1d: code cleaning

--- tests/test_profile1d.py
+++ tests/test_profile1d.py
@@ -7,6 +7,7 @@ from test_utils import PerformTest
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 from smoderp2d import Runner
 
+
 class TestCmd:
     def test_001_roff(self):
         os.environ["SMODERP2D_PROFILE1D"] = "1"
