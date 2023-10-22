c8d5e79e8d226d1c314bb90c29f81f1206807b7d
ondrej <pesej.ondrek@gmail.com>
2023-10-22 14:35:36 +0200

    pywps: no need to remember variable dirs

--- bin/wps/pywps/processes/smoderp2d.py
+++ bin/wps/pywps/processes/smoderp2d.py
@@ -42,7 +42,7 @@ surface and subsurface runoff and erosion
     def process_output(outdir):
         def zipdir(path, ziph):
             # ziph is zipfile handle
-            for root, dirs, files in os.walk(path):
+            for root, _, files in os.walk(path):
                 for file in files:
                     ziph.write(os.path.join(root, file))
 
