72c25a097e31414a97abd572e00535da11b92beb
jerabekjak <jerabekjaku@gmail.com>
2023-10-27 13:30:04 +0200

    Update smoderp2d/exceptions.py
    Co-authored-by: Martin Landa <landa.martin@gmail.com>

--- smoderp2d/exceptions.py
+++ smoderp2d/exceptions.py
@@ -51,7 +51,7 @@ class ConfigError(Exception):
 class SmallParameterValueError(ConfigError):
     """ Exception raised if a parameter reaches a wrong (too small) numeric value
     """
-    def __init__(self,param, value, limit):
+    def __init__(self, param, value, limit):
         self.msg = "Parameter '{}' has low value ({} < {}).".format(
                 param, value, limit
         )
