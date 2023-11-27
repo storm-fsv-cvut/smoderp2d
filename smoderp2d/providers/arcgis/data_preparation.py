58b77d0b0ea357a91507b3c53c63909e34783f1a
Martin Landa <landa.martin@gmail.com>
2023-11-27 09:32:38 +0100

    comment

--- smoderp2d/providers/arcgis/data_preparation.py
+++ smoderp2d/providers/arcgis/data_preparation.py
@@ -24,7 +24,7 @@ class PrepareData(PrepareDataGISBase):
                 "Spatial Analysis extension for ArcGIS is not available"
             )
 
-        arcpy.env.XYTolerance = "0.000001 Meters" # increased because of GRASS tests
+        arcpy.env.XYTolerance = "0.000001 Meters" # increased because of GRASS GIS data comparision
         super(PrepareData, self).__init__(writter)
 
     def _create_AoI_outline(self, elevation, soil, vegetation):
