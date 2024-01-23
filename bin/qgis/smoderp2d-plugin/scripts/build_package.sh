959f274deac45b0d413718d9005df8bd92977174
Martin Landa <landa.martin@gmail.com>
2024-01-23 14:41:38 +0100

    only qgis/grass provider/runner

--- bin/qgis/smoderp2d-plugin/scripts/build_package.sh
+++ bin/qgis/smoderp2d-plugin/scripts/build_package.sh
@@ -9,8 +9,13 @@ echo $script_dir
 pip3 install $script_dir/../../../../ # root directory
 
 LIB=$VENV/lib/python3.11/site-packages
+rm -rf ./smoderp2d
 cp -r $LIB/smoderp2d .
 find smoderp2d -name __pycache__ | xargs rm -rf
+for provider in 'arcgis' 'cmd' 'profile1d' 'wps'; do
+    rm -rv ./smoderp2d/providers/$provider
+    rm -rvf ./smoderp2d/runners/${provider}.py
+done
 
 pb_tool zip
 
