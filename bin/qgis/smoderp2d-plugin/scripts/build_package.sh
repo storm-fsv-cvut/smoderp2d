fdc2d8a98e028067d1d090119e92a9fb822e990e
Martin Landa <landa.martin@gmail.com>
2024-02-10 00:09:44 +0100

    finish scripts/build_package.sh

--- bin/qgis/smoderp2d-plugin/scripts/build_package.sh
+++ bin/qgis/smoderp2d-plugin/scripts/build_package.sh
@@ -6,18 +6,25 @@ source $VENV/bin/activate
 
 script_dir=$(realpath $(dirname $0))
 echo $script_dir
-pip3 install $script_dir/../../../../ # root directory
+pip3 install smoderp2d
 
-LIB=$VENV/lib/python3.11/site-packages
-rm -rf ./smoderp2d
-cp -r $LIB/smoderp2d .
-find smoderp2d -name __pycache__ | xargs rm -rf
+pv=$(python3 -V | cut -d' ' -f 2 | cut -d'.' -f 1,2)
+LIB=$VENV/lib/python$pv/site-packages
+
+pb_tool deploy -y -p zip_build/
+
+plugin_dir=zip_build/smoderp2d_plugin
+cp -r $LIB/smoderp2d $plugin_dir/
+cp -r ../../base $plugin_dir/
+find $plugin_dir/smoderp2d $plugin_dir/base -name __pycache__ | xargs rm -rf
 for provider in 'arcgis' 'cmd' 'profile1d' 'wps'; do
-    rm -rv ./smoderp2d/providers/$provider
-    rm -rvf ./smoderp2d/runners/${provider}.py
+    rm -rv $plugin_dir/smoderp2d/providers/$provider
+    rm -rvf $plugin_dir/smoderp2d/runners/${provider}.py
 done
 
-pb_tool zip
+patch zip_build/smoderp2d_plugin/smoderp_2D_dockwidget.py < patches/smoderp_2D_dockwidget.patch
+
+(cd zip_build; zip -r smoderp2d_plugin.zip smoderp2d_plugin) # pb_tool zip will overwrite patched file
 
 deactivate
 rm -rf $VENV
