93a72b398c7c259a5c9f79fe3ee9d5dcd1cb359a
Ondrej Pesek <pesej.ondrek@gmail.com>
2024-03-02 01:38:32 +0100

    fix fake bifurcation detection (#341)
    only multiple outflows from one stream is considered bifurcation, multiple inflows are fine
    
    see #339

--- smoderp2d/providers/grass/data_preparation.py
+++ smoderp2d/providers/grass/data_preparation.py
@@ -564,7 +564,9 @@ class PrepareData(PrepareDataGISBase):
                 _, end = seg.nodes()
                 seg.attrs[next_down_fieldname] = Globals.streamsNextDownIdNoSegment
                 for start_seg in end.lines():
-                    if start_seg.id != seg.id:
+                    if start_seg.id != seg.id and start_seg[0] == seg[-1]:
+                        # start_seg[0] == seg[-1] -> check only outflowing
+                        # streams, not inflowing
                         if seg.attrs[next_down_fieldname] != Globals.streamsNextDownIdNoSegment:
                             raise DataPreparationError(
                                 'Incorrect stream network topology downstream '
