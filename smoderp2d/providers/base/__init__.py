52ade3ebba8c245a565a3f02e80efe19cb89f9b3
Martin Landa <landa.martin@gmail.com>
2024-04-29 15:38:44 +0200

    fix profile1d provider

--- smoderp2d/providers/base/__init__.py
+++ smoderp2d/providers/base/__init__.py
@@ -521,7 +521,7 @@ class BaseProvider(object):
 
         return data
 
-    def postprocessing(self, cumulative, surface_array, stream, inflows):
+    def postprocessing(self, cumulative, surface_array, stream, inflows=None):
         """Perform postprocessing steps. Store results.
 
         :param cumulative: Cumulative object
