d1e01acca03a5d95819515c1ee4d6abc70b31d67
jerabekjak <jerabekjaku@gmail.com>
2024-05-07 11:59:11 +0200

    documentation of unsaturated hydraulic conductivity funcion

--- smoderp2d/processes/subsurface.py
+++ smoderp2d/processes/subsurface.py
@@ -1,6 +1,11 @@
-def relative_unsat_conductivity(S, l, m):
-
+def relative_unsat_conductivity(S, l=0.5, m):
+    """
+    :param S: subsoil layers saturation.
+    :param l: Mualem's parameter, it is usually 0.5
+    :param vg_m: van genuchten parameter 
+    """
     return S**l * (1.0 - (1.0 - S**(1.0 / m))**m)**2.0
+    
 
 
 def darcy(sub, effect_vrst):
