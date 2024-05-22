fa78da734b6a8d06f14290546b0c60f19fbf11e3
landam <landa.martin@gmail.com>
2024-05-22 16:12:11 +0200

    add support for QGIS standalone installation

--- tests/batch/init_windows_env.bat
+++ tests/batch/init_windows_env.bat
@@ -1,9 +1,14 @@
 @echo off
 
-call "C:\OSGeo4W\bin\o4w_env.bat"
+rem define installation directory
+set INSTALL_DIR=C:\OSGeo4W
+rem set INSTALL_DIR=C:\Program Files\QGIS 3.36.2\
+
+rem GRASS version
 set GRASS_VERSION=83
 
 rem set GRASS GIS environment
+call "%INSTALL_DIR%\bin\o4w_env.bat"
 call "%OSGEO4W_ROOT%\apps\grass\grass%GRASS_VERSION%\etc\env.bat"
 path %OSGEO4W_ROOT%\apps\grass\grass%GRASS_VERSION%\lib;%OSGEO4W_ROOT%\apps\grass\grass%GRASS_VERSION%\bin;%PATH%
 
