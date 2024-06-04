1f085c320501c0ed690c95c073560a680cad3f44
Martin Landa <landa.martin@gmail.com>
2024-06-04 14:44:03 +0200

    improve parallelization of batch processing

--- tests/batch/batch_process_csv_linux.sh
+++ tests/batch/batch_process_csv_linux.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+grass --tmp-location EPSG:5514 --exec g.extension extension=r.hydrodem
+
 PYTHONPATH=`pwd` python3 ./bin/grass/batch_process_csv.py \
           --csv ./tests/batch/batch_process.csv \
           --workers 1
