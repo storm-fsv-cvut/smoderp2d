53894a3b26b25d56c8455d3a231870fe7eafbf5b
pesekon2 <pesej.ondrek@gmail.com>
2025-11-07 12:43:20 +0100

    qgis/build_package: add some comments

--- bin/qgis/smoderp2d-plugin/scripts/build_package.sh
+++ bin/qgis/smoderp2d-plugin/scripts/build_package.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+# create virtual environment
 VENV=/tmp/smoderp2d_plugin
 python3 -m venv $VENV
 source $VENV/bin/activate
@@ -14,18 +15,22 @@ echo $script_dir
 pv=$(python3 -V | cut -d' ' -f 2 | cut -d'.' -f 1,2)
 LIB=$VENV/lib/python$pv/site-packages
 
+# create zip
 pb_tool deploy -y -p zip_build/
 
 plugin_name=smoderp2d_plugin
 plugin_dir=zip_build/$plugin_name
 cp -r $LIB/smoderp2d $plugin_dir/
 cp -r ../../base $plugin_dir/
+
+# remove files and dirs that are unnecessary in the plugin
 find $plugin_dir/smoderp2d $plugin_dir/base -name __pycache__ | xargs rm -rf
 for provider in 'arcgis' 'cmd' 'profile1d' 'wps'; do
     rm -rv $plugin_dir/smoderp2d/providers/$provider
     rm -rvf $plugin_dir/smoderp2d/runners/${provider}.py
 done
 
+# apply patch
 patch zip_build/${plugin_name}/smoderp_2D_dockwidget.py < patches/smoderp_2D_dockwidget.patch
 
 # copy LICENSE file
